c17789306f3333dec37b6f28bd5d10bec182a378
ah2727 <alihd7@proton.me>
2026-03-23 16:53:07 +0330

    add ui_helpers to commands

--- src/mana_analyzer/commands/__init__.py
+++ src/mana_analyzer/commands/__init__.py
@@ -5,7 +5,8 @@ Import side-effects to register optional Typer commands.
 
 # Optional command module: do not crash if a build omits this file.
 try:
-    from .show_flow_cmd import *  # noqa: F401,F403
+    from .cli import *  # noqa: F401,F403
+    from .ui_helpers import *
 except ModuleNotFoundError:
     pass
 
